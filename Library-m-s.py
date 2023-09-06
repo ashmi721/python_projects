f06c91694262069db282b80131c21878f39238bc
ashmi721 <ashmitak721@gmail.com>
2023-09-06 23:22:28 +0545

    make the simple library management system using python

---
+++ Library-m-s.py
@@ -0,0 +1,3 @@
+# write a library class with no_of_books and books as two instance variables. Write a program to create a library from this 
+# class and show how you can print all books, add a book and get the number of books using different methods, show that your program doesn't persist the 
+# books after the program is stopped!
