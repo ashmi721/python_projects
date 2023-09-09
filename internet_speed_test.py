a43f50e9466405cd31c983f5bd8a129b7090bc13
ashmi721 <ashmitak721@gmail.com>
2023-09-09 20:55:43 +0545

    add small user interface

--- internet_speed_test.py
+++ internet_speed_test.py
@@ -7,9 +7,18 @@ sp.geometry("500x500")
 sp.config(bg="Blue")
 
 lab = Label(sp,text="Internet Speed Test", font=("Time New Roman",20,"bold"),bg="blue",fg="white")
-lab.place(x=70,y=30)
+lab.place(x=60,y=40,height= 50,width=380)
 
 lab = Label(sp,text="Download Speed", font=("Time New Roman",20,"bold"),bg="blue",fg="white")
-lab.place(x=70,y=70)
+lab.place(x=60,y=130,height= 50,width=380)
+
+lab = Label(sp,text="00", font=("Time New Roman",20,"bold"),bg="blue",fg="white")
+lab.place(x=60,y=200,height= 50,width=380)
+
+lab = Label(sp,text="Upload Speed", font=("Time New Roman",20,"bold"),bg="blue",fg="white")
+lab.place(x=60,y=290,height= 50,width=380)
+
+lab = Label(sp,text="00", font=("Time New Roman",20,"bold"),bg="blue",fg="white")
+lab.place(x=60,y=340,height= 50,width=380)
 
 sp.mainloop()
