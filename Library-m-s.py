8b7b81af9a4e0dfa3baf9551cc32c14fd9d26a5a
ashmi721 <ashmitak721@gmail.com>
2023-10-04 16:26:18 +0545

    finally complete the coding

--- Library-m-s.py
+++ Library-m-s.py
@@ -2,26 +2,46 @@
  class and show how you can print all books, add a book and get the number of books using different methods, show that your program doesn't persist the 
  books after the program is stopped!
 '''
-class library:
+class Library:
     def __init__(self):
-        self.no_of_books = 0
-        self.books=[]
-    
-    def add_book(self,book):
-        self.books.append(book)
-        self.no_of_books = len(self.books)
+        self.no_of_books = 0 # Initialize the number of books to 0
+        self.books = {}       # Initialize the dictionary of books
 
+    def add_book(self, book, category): # create add book funtion with two parameter
+        self.books[book] = category  # add book with category in dectinary of books
+        self.no_of_books += 1
 
-    def remove_book(self,book):
+    def remove_book(self, book):
         if book in self.books:
-            self.books.remove(book) 
-            self.no_of_books = len(self.books)
+            del self.books[book]
+            self.no_of_books -= 1
 
-    def show_info(self):
-        print(f"Number of books in the library are {self.no_of_books} :")
-        for book in self.books:
-            print(book)
+    def print_books(self):
+        print("Available books in the library are:",self.no_of_books)
+        for book, category in self.books.items():
+            print(f"{book} - {category}")
 
-l1 = library()
-l1.add_book("Harry Potter")
-l1.show_info()
+    def get_no_of_books(self):
+        return self.no_of_books
+
+l1 = Library()
+l1.add_book("Harry Potter", "Fantasy")
+print(f"Number of Initial books: {l1.get_no_of_books()}")
+
+while True:
+    print("Enter a book name (Enter 'a' to add, 'q' to quit, 'r' to remove get the total info):")
+    choice = input()
+    if choice == 'q':
+        break
+    elif choice == 'a':
+        print("Enter a book name:")
+        book = input()
+        print("Enter the category of the book:")
+        category = input()
+        l1.add_book(book, category)
+    elif choice == 'r':
+        print("Enter the name of the book to remove:")
+        book_to_remove = input()
+        l1.remove_book(book_to_remove)
+
+l1.print_books()
