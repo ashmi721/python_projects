43e6922cc21fbb25a8f860ca285fb35c60b07e54
ashmi721 <ashmitak721@gmail.com>
2023-11-22 21:01:11 +0545

    using the pythoon colorsys library with turtle to make an animation

---
+++ animation.py
@@ -0,0 +1,21 @@
+import colorsys
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor('black')
+
+t.speed(100)
+
+n = 36
+h =0
+
+for i in range(460):
+    c = colorsys.hsv_to_rgb(h,1,0.8)
+    h+=1/n
+    t.color(c)
+    t.left(145)
+    
+    for j in range(5):
+        t.forward(300)
+        t.left(150)
